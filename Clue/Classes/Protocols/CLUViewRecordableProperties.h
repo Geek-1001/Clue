aae7b4f35980df99477fb0ad43995610bc9a0b60
Geek-1001 <demhasul@gmail.com>
2016-05-27 10:23:30 +0300

    Add protocol for view to return readable properties dictionary (CLURecordableProperties) for View Structure recordable module

---
+++ Clue/Classes/Protocols/CLUViewRecordableProperties.h
@@ -0,0 +1,16 @@
+//
+//  CLUViewRecordableProperties.h
+//  Clue
+//
+//  Created by Ahmed Sulaiman on 5/27/16.
+//  Copyright © 2016 Ahmed Sulaiman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol CLUViewRecordableProperties <NSObject>
+
+@required
+- (NSMutableDictionary *)clue_viewPropertiesDictionary;
+
+@end
