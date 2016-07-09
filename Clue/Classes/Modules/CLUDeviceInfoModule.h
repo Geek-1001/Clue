256190d54864ea9d5d153150a2558e07f25eec79
Geek-1001 <demhasul@gmail.com>
2016-07-09 12:41:42 +0300

    Add Device Info Module to record information about current device (only once)

---
+++ Clue/Classes/Modules/CLUDeviceInfoModule.h
@@ -0,0 +1,14 @@
+//
+//  CLUDeviceInfoModule.h
+//  Clue
+//
+//  Created by Ahmed Sulaiman on 7/9/16.
+//  Copyright © 2016 Ahmed Sulaiman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "CLUInfoModule.h"
+
+@interface CLUDeviceInfoModule : NSObject <CLUInfoModule>
+
+@end
---
+++ Clue/Classes/Modules/CLUDeviceInfoModule.m
@@ -0,0 +1,58 @@
+//
+//  CLUDeviceInfoModule.m
+//  Clue
+//
+//  Created by Ahmed Sulaiman on 7/9/16.
+//  Copyright © 2016 Ahmed Sulaiman. All rights reserved.
+//
+
+#import "CLUDeviceInfoModule.h"
+#import "CLUDataWriter.h"
+#import <UIKit/UIKit.h>
+
+@interface CLUDeviceInfoModule()
+@property (nonatomic) CLUDataWriter *writer;
+@end
+
+@implementation CLUDeviceInfoModule
+
+- (instancetype)initWithWriter:(CLUDataWriter *)writer {
+    self = [super init];
+    if (!self || !writer) {
+        return nil;
+    }
+    _writer = writer;
+    return self;
+}
+
+- (void)recordInfoData {
+    NSDictionary *deviceProperties = [self deviceProperties];
+    if ([NSJSONSerialization isValidJSONObject:deviceProperties]) {
+        NSError *error;
+        NSData *deviceData = [NSJSONSerialization dataWithJSONObject:deviceProperties options:0 error:&error];
+        if (!error && deviceData) {
+            [_writer startWriting];
+            [_writer addData:deviceData];
+            [_writer finishWriting];
+        }
+    } else {
+        NSLog(@"Device properties json is invalid");
+    }
+}
+
+- (NSDictionary *)deviceProperties {
+    NSMutableDictionary *deviceProperties = [[NSMutableDictionary alloc] init];
+    UIDevice *device = [UIDevice currentDevice];
+    
+    [deviceProperties setObject:device.name forKey:@"name"];
+    [deviceProperties setObject:device.systemName forKey:@"systemName"];
+    [deviceProperties setObject:device.systemVersion forKey:@"systemVersion"];
+    [deviceProperties setObject:device.model forKey:@"model"];
+    [deviceProperties setObject:device.identifierForVendor ? [device.identifierForVendor UUIDString] : @"" forKey:@"identifierForVendor"];
+    [deviceProperties setObject:@(device.batteryLevel) forKey:@"batteryLevel"];
+    [deviceProperties setObject:@(device.batteryState) forKey:@"batteryState"];
+    
+    return deviceProperties;
+}
+
+@end
