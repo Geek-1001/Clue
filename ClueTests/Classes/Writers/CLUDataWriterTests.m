bf3cac781d4cc7516d8d0382c213067ce13d4a4e
Geek-1001 <demhasul@gmail.com>
2017-03-17 23:12:56 +0200

    Add setup and tearDown method to Data Writer tests, remove copy-paste code from tests and move it to setup and tearDown methods

--- ClueTests/Classes/Writers/CLUDataWriterTests.m
+++ ClueTests/Classes/Writers/CLUDataWriterTests.m
@@ -11,70 +11,73 @@
 
 @interface CLUDataWriterTests : XCTestCase
 
+@property (nonatomic) NSString *testContent;
+@property (nonatomic) NSFileManager *fileManager;
+@property (nonatomic) NSURL *testOutputURL;
+@property (nonatomic) CLUDataWriter *dataWriter;
+
 @end
 
 @implementation CLUDataWriterTests
 
-- (void)testAddDataWithIncorrectInput {
+- (void)setUp {
+    [super setUp];
     // Initialize test variables
-    NSFileManager *fileManager = [NSFileManager defaultManager];
-    NSURL *testOutputURL = [NSURL fileURLWithPath:@"test-file"];
+    _fileManager = [NSFileManager defaultManager];
+    _testOutputURL = [NSURL fileURLWithPath:@"test-file"];
+    _testContent = @"Test Data String Content";
     
     // Initialize Data Writer
-    CLUDataWriter *dataWriter = [[CLUDataWriter alloc] initWithOutputURL:testOutputURL];
-    
+    _dataWriter = [[CLUDataWriter alloc] initWithOutputURL:_testOutputURL];
+}
+
+- (void)tearDown {
+    NSError *error;
+    [_fileManager removeItemAtURL:_testOutputURL error:&error];
+    XCTAssertNil(error, @"Unexpected error while removing Test Output File");
+    _fileManager = nil;
+    _dataWriter = nil;
+    _testOutputURL = nil;
+    [super tearDown];
+}
+
+- (void)testAddDataWithIncorrectInput {
     // Start Writing and add nil data
-    [dataWriter startWriting];
-    [dataWriter addData:nil];
-    [dataWriter finishWriting];
+    [_dataWriter startWriting];
+    [_dataWriter addData:nil];
+    [_dataWriter finishWriting];
     
     // Test is output file exists
-    BOOL isOutputFileExists = [fileManager fileExistsAtPath:testOutputURL.path];
+    BOOL isOutputFileExists = [_fileManager fileExistsAtPath:_testOutputURL.path];
     XCTAssertTrue(isOutputFileExists, @"Test Output File doesn't exists");
     
     // Test  if outout file is empty
-    NSData *outputFileData = [NSData dataWithContentsOfURL:testOutputURL];
+    NSData *outputFileData = [NSData dataWithContentsOfURL:_testOutputURL];
     XCTAssertEqual(outputFileData.length, 0, @"Contents length from Test Output File is incorrect");
-    
-    // Remove Test Output File
-    NSError *error;
-    [fileManager removeItemAtURL:testOutputURL error:&error];
-    XCTAssertNil(error, @"Unexpected error while removing Test Output File");
 }
 
 - (void)testAddDataWithCorrectInput {
     // Initialize/Declare test variables
-    NSFileManager *fileManager = [NSFileManager defaultManager];
-    NSString *testContent = @"Test Data String Content";
-    NSURL *testOutputURL = [NSURL fileURLWithPath:@"test-file"];
-    NSData *testInputData = [testContent dataUsingEncoding:NSUTF8StringEncoding];
-    NSData *testOutputData = [[testContent stringByAppendingString:@"\n"] dataUsingEncoding:NSUTF8StringEncoding];
-    
-    // Initialize Data Writer
-    CLUDataWriter *dataWriter = [[CLUDataWriter alloc] initWithOutputURL:testOutputURL];
+    NSData *testInputData = [_testContent dataUsingEncoding:NSUTF8StringEncoding];
+    NSData *testOutputData = [[_testContent stringByAppendingString:@"\n"] dataUsingEncoding:NSUTF8StringEncoding];
     
     // Start writinf process and add valid data
-    [dataWriter startWriting];
-    [dataWriter addData:testInputData];
-    [dataWriter finishWriting];
+    [_dataWriter startWriting];
+    [_dataWriter addData:testInputData];
+    [_dataWriter finishWriting];
     
     // Test if output file exists
-    BOOL isOutputFileExists = [fileManager fileExistsAtPath:testOutputURL.path];
+    BOOL isOutputFileExists = [_fileManager fileExistsAtPath:_testOutputURL.path];
     XCTAssertTrue(isOutputFileExists, @"Test Output File doesn't exists");
     
     // Test if length in output file is equal to test output data
-    NSData *outputFileData = [NSData dataWithContentsOfURL:testOutputURL];
+    NSData *outputFileData = [NSData dataWithContentsOfURL:_testOutputURL];
     XCTAssertEqual(outputFileData.length, testOutputData.length, @"Contents length from Test Output File is incorrect");
     
     // Test if content of output file is equal to test output data
     NSString *outputFileString = [[NSString alloc] initWithData:outputFileData encoding:NSUTF8StringEncoding];
     NSString *testOutputFileString = [[NSString alloc] initWithData:testOutputData encoding:NSUTF8StringEncoding];
     XCTAssertTrue([outputFileString isEqualToString:testOutputFileString], @"Content from Test Output File is incorrect");
-    
-    // Remove test output file
-    NSError *error;
-    [fileManager removeItemAtURL:testOutputURL error:&error];
-    XCTAssertNil(error, @"Unexpected error while removing Test Output File");
 }
 
 @end
