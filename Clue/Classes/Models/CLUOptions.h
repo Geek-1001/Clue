5d67b08a8347395a8fc6cc257e7b4c5ab7c06c0f
Geek-1001 <demhasul@gmail.com>
2017-03-25 23:45:28 +0200

    Add header docs for Models classes

--- Clue/Classes/Models/CLUOptions.h
+++ Clue/Classes/Models/CLUOptions.h
@@ -8,10 +8,28 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ `CLUOptions` is a data model class to store all configurational variables during Clue framework setup and enable process.
+ For now only `email` property is available. 
+ If you need more configurational property which could be used during first setup - just subclass this class
+ 
+ Used in `ClueController` configuration
+ @code [[ClueController sharedInstance] enableWithOptions:<CLUOptions here>];
+ */
 @interface CLUOptions : NSObject
 
+/**
+ Email address where user will send an email with recorded report attached to it.
+ */
 @property (nonatomic) NSString *email;
 
+/**
+ Create new `CLUOptions` instance with email adress
+ 
+ @param email Email address where user will send an email with recorder report in attachment.
+ 
+ @return instance of `CLUOptions`
+ */
 + (instancetype)initWithEmail:(NSString *)email;
 
 @end
--- Clue/Classes/Models/CLUTouch.h
+++ Clue/Classes/Models/CLUTouch.h
@@ -9,12 +9,33 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
+/**
+ @brief `CLUTouch` is a proxy for UITouch data and properties dictionary generator for this data.
+ 
+ @discussion `CLUTouch` is a data model to store UITouch's data in appropriate way. Since the system reuse UITouch object with each user interaction we can't just store in in out collection in order to generate their properties data dictionary (via UITouch category) and add this data to final report file. Instead we have to transfer UITouch values to our custom class `CLUTouch` and interaction with it.
+ 
+ Used in `CLUGeneralGestureRecognizer` in order to transfer UITouch to CLUTouch.  Also `CLUInteractionObserverDelegate` protocol requires CLUTouch instead of UITouch
+ */
 @interface CLUTouch : NSObject
 
 @property (nonatomic, readonly) CGPoint locationInWindow;
 @property (nonatomic, readonly) NSInteger tapCount;
 
+/**
+ Create new instance of CLUTouch object with data from UITouch object
+ 
+ @param touch system generated UITouch object (usually from touchBegan: touchMoved: or touchEnded: methods)
+ 
+ @return CLUTouch instance with data from UITouch object
+ */
 - (instancetype)initWithTouch:(UITouch *)touch;
+
+/**
+ Generate serialized properties dictionary with all required values there `locationInWindow` and `tapCount`
+ To use this dictionary in final report writing process
+ 
+ @return dictionary with properties of `CLUTouch`
+ */
 - (NSDictionary *)properties;
 
 @end
