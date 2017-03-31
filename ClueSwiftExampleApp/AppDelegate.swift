2b41c3f689e1bf4289bc0e9158bc8d02a2f54d3b
Geek-1001 <demhasul@gmail.com>
2017-04-01 02:59:57 +0300

    Add swift Master-Detail demo project to show how Clue could work with Swift and transition between different screens.

---
+++ ClueSwiftExampleApp/AppDelegate.swift
@@ -0,0 +1,43 @@
+//
+//  AppDelegate.swift
+//  ClueSwiftExampleApp
+//
+//  Created by Ahmed Sulaiman on 4/1/17.
+//  Copyright © 2017 Ahmed Sulaiman. All rights reserved.
+//
+
+import UIKit
+import Clue
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        let splitViewController = window!.rootViewController as! UISplitViewController
+        let navigationController = splitViewController.viewControllers[splitViewController.viewControllers.count-1] as! UINavigationController
+        navigationController.topViewController!.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem
+        splitViewController.delegate = self
+        
+        // Enable Clue
+        ClueController.sharedInstance().enable(with: CLUOptions(email: "ahmed.sulajman@gmail.com"))
+        
+        return true
+    }
+
+    // MARK: - Split view
+
+    func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController:UIViewController, onto primaryViewController:UIViewController) -> Bool {
+        guard let secondaryAsNavController = secondaryViewController as? UINavigationController else { return false }
+        guard let topAsDetailController = secondaryAsNavController.topViewController as? DetailViewController else { return false }
+        if topAsDetailController.detailItem == nil {
+            // Return true to indicate that we have handled the collapse by doing nothing; the secondary controller will be discarded.
+            return true
+        }
+        return false
+    }
+
+}
+
