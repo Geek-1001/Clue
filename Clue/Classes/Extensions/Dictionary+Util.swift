8796731786b1215f525cf3085d7f426cc6ac885a
Andrea Prearo <andrea.prearo@gmail.com>
2017-05-09 07:42:44 -0700

    Add Dictionary extension methods
    Changes:
    - Implement Dictionary extension methods in Dictionary+Util.

---
+++ Clue/Classes/Extensions/Dictionary+Util.swift
@@ -0,0 +1,24 @@
+//
+//  Dictionary+Util.swift
+//  Clue
+//
+//  Created by Prearo, Andrea on 5/8/17.
+//  Copyright © 2017 Ahmed Sulaiman. All rights reserved.
+//
+
+import Foundation
+
+extension Dictionary {
+    /// Returns a new dictionary containing the entries of this instance and another dictionary passed as a parameter.
+    ///
+    /// - Parameter other: The dictionary to use for the merge operation.
+     
+    /// - Returns: A new dictionary containing the entries of this instance and another dictionary passed as a parameter.
+    func merged(with other: Dictionary) -> Dictionary {
+        var returnDictionary = self
+        for (key, value) in other {
+            returnDictionary[key] = value
+        }
+        return returnDictionary
+    }
+}
